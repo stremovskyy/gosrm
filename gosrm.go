63ca69e57ec60050ad4dafc1c233b06611ccd945
Anthony <stremovskyy@me.com>
2018-08-31 18:20:58 +0300

    RouteRequest struct pointer fix revert commit

--- gosrm.go
+++ gosrm.go
@@ -36,22 +36,22 @@ type RouteRequest struct {
 	Coordinates geo.PointSet `json:"coordinates"`
 
 	// Returned route steps for each route leg
-	Steps bool `json:"steps"`
+	Steps *bool `json:"steps"`
 
 	// Search for alternative routes. Passing a number alternatives=n searches for up to  n alternative routes
-	Alternatives string `json:"alternatives"`
+	Alternatives *string `json:"alternatives"`
 
 	// Returns additional metadata for each coordinate along the route geometry
-	Annotations string `json:"annotations"`
+	Annotations *string `json:"annotations"`
 
 	// Returned route geometry format (influences overview and per step)
-	Geometries string `json:"geometries"`
+	Geometries *string `json:"geometries"`
 
 	// Add overview geometry either full, simplified according to highest zoom level it could be display on, or not at all.
-	Overview string `json:"overview"`
+	Overview *string `json:"overview"`
 
 	// Forces the route to keep going straight at waypoints constraining uturns there even if it would be faster. Default value depends on the profile.
-	ContinueStraight string `json:"continue_straight"`
+	ContinueStraight *string `json:"continue_straight"`
 }
 
 // Snaps a coordinate to the street network and returns the nearest n matches.
@@ -69,10 +69,10 @@ type TableRequest struct {
 	Coordinates geo.PointSet `json:"coordinates"`
 
 	// Use location with given index as source.
-	Sources []int `json:"sources"`
+	Sources *[]int `json:"sources"`
 
 	// Use location with given index as destination.
-	Destinations []int `json:"destinations"`
+	Destinations *[]int `json:"destinations"`
 }
 
 type MatchRequest struct {
